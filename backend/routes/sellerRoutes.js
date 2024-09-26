a30214f52fe1768888e70c617fb701b0487035f9
Ahmed Abolfadl <99916062+abolfadlinho@users.noreply.github.com>
2024-09-26 19:31:03 +0300

    Preliminary Routes

---
+++ backend/routes/sellerRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const sellerController = require('../controllers/sellerController');
+
+router.post('/', sellerController.createSeller);
+
+router.get('/', sellerController.getAllSellers);
+
+router.get('/:id', sellerController.getSellerById);
+
+router.patch('/:id', sellerController.updateSeller);
+
+router.delete('/:id', sellerController.deleteSeller);
+
+module.exports = router;
