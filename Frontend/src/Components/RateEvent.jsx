f41395be4f025a1374b516d2bd13778040141fff
mariemmobarak <mariem.mobarak@student.edu.eg>
2024-09-29 23:59:09 +0300

    Added Ratings

---
+++ Frontend/src/Components/RateEvent.jsx
@@ -0,0 +1,11 @@
+import CustomRate from "./Components/Rate";
+
+const RateTourGuide = () => {
+    return (
+        <div className="TourGuideRate">
+        <h1>Rate Activity/Event</h1>
+        <CustomRate />
+        </div>
+    );
+}
+export default RateTourGuide;
