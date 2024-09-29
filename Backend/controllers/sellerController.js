0be9722ca4e587c46700fd7a67163d41b5b3bb5c
Tatos23 <128411826+Tatos23@users.noreply.github.com>
2024-09-29 14:43:31 +0300

    updated the sellerUpdateProfile method to exclude some fields

--- Backend/controllers/sellerController.js
+++ Backend/controllers/sellerController.js
@@ -160,8 +160,19 @@ exports.sellerReadProfile = async (req, res) => {
 
 // Seller update profile
 exports.sellerUpdateProfile = async (req, res) => {
+  // Allowed fields for update
+  const allowedFields = ['username', 'email', 'pass', 'id', 'taxationRegCard', 'name', 'desc'];
+
+  // Filter the request body
+  const filteredBody = {}; 
+  allowedFields.forEach(field => {  // Loop through the allowed fields
+    if (req.body[field] !== undefined) { // Check if the field exists in the request body
+      filteredBody[field] = req.body[field]; // Add the field to the filtered body
+    }
+  });
+
   try {
-    const { pass, ...otherFields } = req.body;
+    const { pass, ...otherFields } = filteredBody; // Destructure the password from the filtered body
 
     // Retrieve the existing seller document
     const existingSeller = await Seller.findById(req.params.id);
@@ -170,21 +181,21 @@ exports.sellerUpdateProfile = async (req, res) => {
     }
 
     // If a new password is provided, compare it with the existing hashed password
-    if (pass) {
+    // if (pass) {
 
-      // Check if the new password is the same as the old password
-      // const isMatch = await bcrypt.compare(pass, existingSeller.pass);
-      // if (isMatch) {
-      //   return res.status(400).json({ message: 'New password cannot be the same as old password' });
-      // }
+    //   // Check if the new password is the same as the old password
+    //   const isMatch = await bcrypt.compare(pass, existingSeller.pass);
+    //   if (isMatch) {
+    //     return res.status(400).json({ message: 'New password cannot be the same as old password' });
+    //   }
 
-      // Hash the new password before updating
-      const salt = await bcrypt.genSalt(10);
-      req.body.pass = await bcrypt.hash(pass, salt);
-    }
+    //   // Hash the new password before updating
+    //   const salt = await bcrypt.genSalt(10);
+    //   filteredBody.pass = await bcrypt.hash(pass, salt);
+    // }
 
     // Proceed with the update
-    const updatedSeller = await Seller.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    const updatedSeller = await Seller.findByIdAndUpdate(req.params.id, filteredBody, { new: true, runValidators: true });
     // runValidators: true is used to ensure that the updated document passes the schema validation rules
     if (!updatedSeller) {
       return res.status(404).json({ message: 'Seller not found' });
