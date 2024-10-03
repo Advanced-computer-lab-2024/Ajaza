9b0e98e04320a05ec660e0bf1190dae3a36a271f
Omar Orensa <omar.orensa319@gmail.com>
2024-10-03 19:51:04 +0300

    -modified login -re44 done -req83 done

--- Backend/models/Venue.js
+++ Backend/models/Venue.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const venueSchema = new mongoose.Schema({
-  governorId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Governor' }, // Reference to the governor who created the venue
+  governorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Governor",
+  }, // Reference to the governor who created the venue
   name: { type: String, required: true }, // Name of the venue
   desc: { type: String, required: true }, // Description of the venue
-  pictures: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Img'}], // Array of picture paths or URLs
+  pictures: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Img" },
+  ], // Array of picture paths or URLs
   location: { type: String, required: true }, // Google Maps link or address
   openingHours: {
     suno: { type: Number, required: true }, // Opening hour on Sunday
@@ -20,17 +26,22 @@ const venueSchema = new mongoose.Schema({
     frio: { type: Number, required: true },
     fric: { type: Number, required: true },
     sato: { type: Number, required: true },
-    satc: { type: Number, required: true }
+    satc: { type: Number, required: true },
   },
   price: {
     foreigner: { type: Number, required: true }, // Price for foreigners
     native: { type: Number, required: true }, // Price for natives
     student: { type: Number, required: true }, // Price for students
   },
-  tags: [{ type: String, enum:['Monuments', 'Museums', 'Religioud Sites', 'Palaces/Castles'] }], // Array of tags related to the venue, from the limited list given in description
+  tags: [
+    {
+      type: String,
+      enum: ["Monuments", "Museums", "Religious Sites", "Palaces/Castles"],
+    },
+  ], // Array of tags related to the venue, from the limited list given in description
 });
 
 // Create the model
-const Venue = mongoose.model('Venue', venueSchema);
+const Venue = mongoose.model("Venue", venueSchema);
 
 module.exports = Venue;
