b81247c3de07c9dbc7c04996438b79e136ebf137
AdamAdham <adamfayek@yahoo.com>
2024-10-06 14:08:02 +0300

    to save progress because han3ok

---
+++ Frontend/src/Components/Tourist/Itineraries.jsx
@@ -0,0 +1,218 @@
+import React, { useEffect, useState } from "react";
+import SearchFilterSortContainer from "../Common/SearchFilterSortContainer";
+import {
+  apiUrl,
+  calculateYourPrice,
+  getAvgRating,
+  comparePriceRange,
+} from "../Common/Constants";
+import axios from "axios";
+import BasicCard from "../Common/BasicCard";
+
+const convertCategoriesToValues = (categoriesArray) => {
+  return categoriesArray.map((categoryObj) => {
+    return {
+      displayName: categoryObj.category,
+      filterCriteria: categoryObj.category,
+    };
+  });
+};
+
+const convertTagsToValues = (tagsArray) => {
+  return tagsArray.map((tagObj) => {
+    return {
+      displayName: tagObj.tag,
+      filterCriteria: tagObj.tag,
+    };
+  });
+};
+
+const Itineraries = () => {
+  const [combinedElements, setCombinedElements] = useState([]);
+
+  // propName:fieldName
+  const propMapping = {
+    title: "name",
+    extra: "price",
+    rating: "avgRating",
+    dateTime: "availableDateTime",
+  };
+  const fields = {
+    Language: "language",
+    "Accessibility Offered": "accessibility",
+    "Pick Up": "pickUp",
+    "Drop off": "dropOff",
+  };
+  const searchFields = ["name"];
+  const constProps = { rateDisplay: true };
+  const sortFields = ["avgRating", "price"];
+  const [filterFields, setfilterFields] = useState({
+    tags: {
+      displayName: "Tags",
+      values: [
+        { displayName: "kayaking", filterCriteria: "kayaking" },
+        { displayName: "history", filterCriteria: "history" },
+        { displayName: "mountain", filterCriteria: "mountain" },
+      ],
+      compareFn: (filterCriteria, element) => {
+        if (!element.tags) {
+          return false;
+        }
+        // filterCriteria is one of the values that we can filter with
+        return element?.tags.includes(filterCriteria);
+      },
+    },
+    price: {
+      displayName: "Price",
+      values: [
+        { displayName: "0-20", filterCriteria: [0, 20] },
+        { displayName: "21-60", filterCriteria: [21, 60] },
+        { displayName: "61-100", filterCriteria: [61, 100] },
+        { displayName: "101+", filterCriteria: [101, 9999999999] },
+      ],
+      compareFn: (filterCriteria, element) =>
+        comparePriceRange(filterCriteria, element),
+    },
+    language: {
+      displayName: "Languages",
+      values: [
+        { displayName: "Arabic", filterCriteria: "Arabic" },
+        { displayName: "English", filterCriteria: "English" },
+        { displayName: "Spanish", filterCriteria: "Spanish" },
+        { displayName: "German", filterCriteria: "German" },
+        { displayName: "Italian", filterCriteria: "Italian" },
+        { displayName: "French", filterCriteria: "French" },
+      ],
+      compareFn: (filterCriteria, element) => {
+        if (!element.language) {
+          return false;
+        }
+        // filterCriteria is one of the values that we can filter with
+        return element?.language.includes(filterCriteria);
+      },
+    },
+    availableDateTime: {
+      displayName: "Available Dates/Times",
+      values: [
+        {
+          displayName: "January",
+          filterCriteria: ["2024-01-01", "2024-01-31"],
+        },
+        {
+          displayName: "February",
+          filterCriteria: ["2024-02-01", "2024-02-29"],
+        },
+        {
+          displayName: "March",
+          filterCriteria: ["2024-03-01", "2024-03-31"],
+        },
+        {
+          displayName: "April",
+          filterCriteria: ["2024-04-01", "2024-04-30"],
+        },
+        {
+          displayName: "May",
+          filterCriteria: ["2024-05-01", "2024-05-31"],
+        },
+        {
+          displayName: "June",
+          filterCriteria: ["2024-06-01", "2024-06-30"],
+        },
+        {
+          displayName: "July",
+          filterCriteria: ["2024-07-01", "2024-07-31"],
+        },
+        {
+          displayName: "August",
+          filterCriteria: ["2024-08-01", "2024-08-31"],
+        },
+        {
+          displayName: "September",
+          filterCriteria: ["2024-09-01", "2024-09-30"],
+        },
+        {
+          displayName: "October",
+          filterCriteria: ["2024-10-01", "2024-10-31"],
+        },
+        {
+          displayName: "November",
+          filterCriteria: ["2024-11-01", "2024-11-30"],
+        },
+        {
+          displayName: "December",
+          filterCriteria: ["2024-12-01", "2024-12-31"],
+        },
+      ],
+      compareFn: (filterCriteria, element) => {
+        // filterCriteria is an array of minmimum and maximum values
+        // Convert startDate and endDate to Date objects
+        const start = new Date(filterCriteria[0]);
+        const end = new Date(filterCriteria[1]);
+        if (!element.availableDateTime) {
+          return false;
+        }
+        // Check if any of the dates in availableDateTime are in the date range
+        return element.availableDateTime.some((available) => {
+          const availableDate = new Date(available.date);
+          return availableDate >= start && availableDate <= end;
+        });
+      },
+    },
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [itineraryResponse, tagResponse] = await Promise.all([
+          axios.get(`${apiUrl}itinerary/notHidden`),
+          axios.get(`${apiUrl}tag`),
+        ]);
+        let itineraries = itineraryResponse.data;
+        let tags = tagResponse.data;
+
+        filterFields.tags = {
+          displayName: "Tags",
+          values: convertTagsToValues(tags),
+          compareFn: (filterCriteria, element) => {
+            if (!element.tags) {
+              return false;
+            }
+            // Convert filterCriteria to lowercase
+            const lowerFilterCriteria = filterCriteria.toLowerCase();
+            // Check if any of the tags (converted to lowercase) match the filterCriteria
+            return element?.tags.some(
+              (tag) => tag.toLowerCase() === lowerFilterCriteria
+            );
+          },
+        };
+
+        let combinedArray = itineraries;
+
+        combinedArray = combinedArray.map((element) => {
+          return { ...element, avgRating: getAvgRating(element.feedback) };
+        });
+
+        setCombinedElements(combinedArray);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <SearchFilterSortContainer
+      cardComponent={BasicCard}
+      elements={combinedElements}
+      propMapping={propMapping}
+      searchFields={searchFields}
+      constProps={constProps}
+      fields={fields}
+      sortFields={sortFields}
+      filterFields={filterFields}
+    />
+  );
+};
+
+export default Itineraries;
