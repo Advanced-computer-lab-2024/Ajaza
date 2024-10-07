de25bb57236b6bd8716d7f11ef6d3d55ed8e89e3
AdamAdham <adamfayek@yahoo.com>
2024-10-07 23:11:46 +0300

    Error logging

--- Frontend/src/Components/Common/SearchFilterSortContainerEditCreate.jsx
+++ Frontend/src/Components/Common/SearchFilterSortContainerEditCreate.jsx
@@ -349,7 +349,7 @@ const SearchFilterSortContainerEditCreate = ({
       setEditingProductId(null);
     } catch (error) {
       message.error(
-        "Failed to create activity." + error?.response?.data?.error
+        "Failed to create Product. " + error?.response?.data?.error
       );
     }
   };
