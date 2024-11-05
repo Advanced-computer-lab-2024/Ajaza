e5a7806771be484bc590881f1bef08d41420f1b0
Tatos23 <128411826+Tatos23@users.noreply.github.com>
2024-11-05 16:37:01 +0200

    fixing the changing URL without checking token and current role problem

--- Frontend/src/Components/TourismGovernor/TourismGovernor.jsx
+++ Frontend/src/Components/TourismGovernor/TourismGovernor.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CustomLayout } from "../Common";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Venues from "../Venues";
 import { CalendarOutlined, ContainerOutlined } from "@ant-design/icons";
 import Profile from "../Common/Profile";
 import ChangePasswordForm from "../Common/changePassword"; 
+import SignIn from "../Sign/SignIn";
+import { jwtDecode } from "jwt-decode";
 
 
 const TourismGovernor = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    console.log("Token:", token);
+    console.log("decodedToken:", jwtDecode(token));
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        // Check if the token is valid and if the user role is 'advertiser'
+        if (!decodedToken || decodedToken.role !== "governor") {
+          navigate("/auth/signin");
+        }
+      } catch (error) {
+        console.error("Invalid token:", error);
+        navigate("/auth/signin");
+      }
+    } else {
+      navigate("/auth/signin");
+    }
+  }, [navigate]);
+
   const sideBarItems = [
     // {
     //   key: "1",
@@ -36,6 +59,7 @@ const TourismGovernor = () => {
         <Route path="/" element={<Venues />} />
         <Route path="profile" element={<Profile />} />
         <Route path="change-password"  element={<ChangePasswordForm />}  />
+        <Route path="auth/signin" element={<SignIn />} />
       </Routes>
     </CustomLayout>
   );
