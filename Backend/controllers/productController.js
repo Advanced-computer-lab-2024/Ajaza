c34910383b5ba665e7b3ee01327d7c504a757063
= <ahmedamr_2003@yahoo.com>
2024-11-10 21:25:39 +0200

    hany shal el 7agat el ana ghayartaha :(

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -16,10 +16,11 @@ exports.createProduct = async (req, res) => {
 
 // Get all products req81
 exports.getAllProducts = async (req, res) => {
+  console.log("reached");
   try {
     const products = await Product.find({hidden: { $ne: true }/*, archived: { $ne: true },*/});
     if(!products || products.length === 0){
-      //return res.status(404).json({ message: "No products found" });
+      return res.status(404).json({ message: "No products found" });
     }
     res.status(200).json(products);
   } catch (error) {
