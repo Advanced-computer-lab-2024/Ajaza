8fca6795bb42ce49ca96825292c7b16108f6676d
Mshaker16 <mohamed.shaker.ms2003@gmail.com>
2024-11-30 19:20:12 +0200

    promo small fix

--- Backend/controllers/promoCodeController.js
+++ Backend/controllers/promoCodeController.js
@@ -1,6 +1,5 @@
-const PromoCode = require('../models/PromoCode');
-const Tourist = require('../models/Tourist');
-
+const PromoCode = require("../models/PromoCode");
+const Tourist = require("../models/Tourist");
 
 // Create a new promo code
 exports.createPromoCode = async (req, res) => {
@@ -28,7 +27,7 @@ exports.getPromoCodeById = async (req, res) => {
   try {
     const promoCode = await PromoCode.findById(req.params.id);
     if (!promoCode) {
-      return res.status(404).json({ message: 'Promo code not found' });
+      return res.status(404).json({ message: "Promo code not found" });
     }
     res.status(200).json(promoCode);
   } catch (error) {
@@ -39,9 +38,13 @@ exports.getPromoCodeById = async (req, res) => {
 // Update promo code by ID
 exports.updatePromoCode = async (req, res) => {
   try {
-    const updatedPromoCode = await PromoCode.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedPromoCode = await PromoCode.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
     if (!updatedPromoCode) {
-      return res.status(404).json({ message: 'Promo code not found' });
+      return res.status(404).json({ message: "Promo code not found" });
     }
     res.status(200).json(updatedPromoCode);
   } catch (error) {
@@ -54,49 +57,47 @@ exports.deletePromoCode = async (req, res) => {
   try {
     const deletedPromoCode = await PromoCode.findByIdAndDelete(req.params.id);
     if (!deletedPromoCode) {
-      return res.status(404).json({ message: 'Promo code not found' });
+      return res.status(404).json({ message: "Promo code not found" });
     }
-    res.status(200).json({ message: 'Promo code deleted successfully' });
+    res.status(200).json({ message: "Promo code deleted successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-exports.checkValid = async (req,res) => {
+exports.checkValid = async (req, res) => {
   try {
     const code = req.params.code;
 
-    if(!code) {
-      return res.status(404).json({message:"Missing code"});
+    if (!code) {
+      return res.status(404).json({ message: "Missing code" });
     }
 
-    const promo = await PromoCode.findOne({code});
+    const promo = await PromoCode.findOne({ code });
 
-    if(!promo) {
-      return res.status(404).json({message: "Promocode not found"});
+    if (!promo) {
+      return res.status(404).json({ message: "Promocode not found" });
     }
-    if(promo.birthday.touristIds.length === 0) {
-      return res.status(200).json({value: promo.value});
+    if (promo.birthday.touristIds.length === 0) {
+      return res.status(200).json({ value: promo.value });
     } else {
-
       const touristId = req.body.touristId;
 
-      if(!touristId) {
-        return res.status(404).json({message:"Tourist ID needed"});
+      if (!touristId) {
+        return res.status(404).json({ message: "Tourist ID needed" });
       }
 
-      if(promo.birthday.touristIds.includes(touristId)) {
-        return res.status(200).json({value: 0.8});
+      if (promo.birthday.touristIds.includes(touristId)) {
+        return res.status(200).json({ value: 0.8 });
       } else {
-        return res.status(404).json({message:"Tourist not eligible"});
+        return res.status(404).json({ message: "Tourist not eligible" });
       }
     }
-
-  } catch(error) {
+  } catch (error) {
     console.log(error);
-    res.status(500).json({message:"Internal error"});
+    res.status(500).json({ message: "Internal error" });
   }
-}
+};
 
 // Admin create a promo code
 exports.createPromoAdmin = async (req, res) => {
@@ -104,12 +105,12 @@ exports.createPromoAdmin = async (req, res) => {
     const { code, value } = req.body;
 
     if (!code || !value) {
-      return res.status(400).json({ message: 'Code and value are required' });
+      return res.status(400).json({ message: "Code and value are required" });
     }
 
     const promoCode = new PromoCode({
       code,
-      value,
+      value: 1 - value,
     });
 
     const savedPromoCode = await promoCode.save();
